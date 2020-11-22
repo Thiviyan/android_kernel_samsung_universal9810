8f18e70554be9ab30219bad4ee8aa847b10a35a2
Thiviyan <Thiviyan@gmail.com>
2020-11-22 19:35:40 +0530

    fixed modules

--- build.sh
+++ build.sh
@@ -10,3 +10,11 @@ make -j$(nproc --all) O=out \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE=aarch64-linux-android- \
                       CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+                      modules_install INSTALL_MOD_PATH=modules_out
